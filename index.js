a781db5a3a3ddbb4cfbb3b8131e15db09e2c331b
Elitel <Alitali@yandex.ru>
2019-03-08 17:45:36 +0300

    public folder

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
