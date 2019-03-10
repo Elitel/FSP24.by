38d96cb2db7f517ee36c2b02dc45ddf58ac986c5
Elitel <Alitali@yandex.ru>
2019-03-10 16:21:28 +0300

    adds

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var nodemailer = require('nodemailer');
+
 var app = express();
+app.set('view engine', 'ejs');
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+//Static folder
+app.use(express.static(__dirname + "/public"));
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.post('/send-email', (req, res) => {
+  var output = `
+  <p>Новый запрос</p>
+  <h3>Информация о клиенте:</h3>
+  <ul>
+    <li>Имя: ${req.body.client-name}</li>
+    <li>Номер телефона: ${req.body.phone-number}</li>
+  </ul>
+  <h3>Информация о неполадке:</h3>
+  <ul>
+    <li>Устройство: ${req.body.divece}</li>
+    <li>Описание неполадки: ${req.body.problem}</li>
+  </ul>
+  `;
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'xxx@xx.com',
+        pass: 'xxxx'
+    }
+});
+let mailOptions = {
+    from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
+    to: req.body.to, // list of receivers
+    subject: req.body.subject, // Subject line
+    text: req.body.body, // plain text body
+    html: '<b>NodeJS Email Tutorial</b>' // html body
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+        res.render('index');
+    });
+});
+
+  
+
+});
 app.listen(port, function () {
-  console.log('App listening on port 3000!');
+  console.log('App listening...');
 });
---
+++ public/js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+    $('.return-to-top').fadeIn(200); // Fade in the arrow
+  } else {
+    $('.return-to-top').fadeOut(200); // Else fade out the arrow
+  }
+});
+$('.return-to-top').click(function () { // When arrow is clicked
+  $('body,html').animate({
+    scrollTop: 0 // Scroll to top of body
+  }, 800);
+});
+
+
+$(document).ready(function () {
+  contsize();
+});
+$(window).bind("resize", function () {
+  contsize();
+});
+
+function contsize() {
+  $('.figcaption').each(function () {
+    var width = $(this).prev().width();
+    $(this).css('width', width);
+    var height = $(this).parent().height() - parseInt($('.services h3').css("font-size")) - parseInt($(this).css("padding-top")) - parseInt($(this).css("line-height")) * 1.5;
+    $(this).css('top', height);
+  })
+};
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  $('.figcaption').click(function () {
+    var clicks = $(this).data('clicks');
+    if (clicks) {
+      var height = $(this).parent().height() - $(this).height() - parseInt($(this).css("padding-bottom"));
+      $(this).css('top', height);
+    } else {
+      var height = $(this).parent().height() - parseInt($('.services h3').css("font-size")) - parseInt($(this).css("padding-top")) - parseInt($(this).css("line-height")) * 1.5;
+      $(this).css('top', height);
+    }
+    $(this).data("clicks", !clicks);
+  });
+} else {
+  $('.figcaption').hover(function () {
+      var height = $(this).parent().height() - $(this).height() - parseInt($(this).css("padding-bottom"));
+      $(this).css('top', height);
+    },
+    function () {
+      var height = $(this).parent().height() - parseInt($('.services h3').css("font-size")) - parseInt($(this).css("padding-top")) - parseInt($(this).css("line-height")) * 1.5;
+      $(this).css('top', height);
+    });
+};
+
+$(document).ready(function()
+{
+  if ($('.navbar').offset().top>50)
+  {
+    $('.navbar').addClass('transparent');
+  }
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.navbar').addClass('transparent');
+    } else {
+      $('.navbar').removeClass('transparent');
+    }
+  });
+})
+
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
